e3ac819a68e42de33d937b560944897f9eefcb9c
chairul sabri <chairulsabri734@gmail.com>
2023-12-25 20:36:07 +0700

    capaign repo update

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,7 @@
 package handler
+
+// tanggap parameter di handler
+// handler ke service
+// service yang menentukan repository mana yang di panggil/call
+// repository : FindAll, FindUserID
+// akses db
